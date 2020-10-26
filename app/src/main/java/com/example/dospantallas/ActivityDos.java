0879ba326074be609f873ff76a768f57cf4ff2b2
V. Emmanuel Molinas <emmanuel@vemmolinas.dev>
2020-10-26 17:45:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dospantallas/ActivityDos.java
@@ -0,0 +1,14 @@
+package com.example.dospantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityDos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dos);
+    }
+}
